80fa32af497435b66c52ffd7319060fcdc946162
Mike <michael.menon@zco.com>
2018-01-28 17:54:28 -0500

    created checks for nil key and value
    Put in check for nil key and value passed to Get/Set. And put in lock for Erase and Clear functions.

--- mcontext.go
+++ mcontext.go
@@ -26,6 +26,9 @@ func init() {
 //set a value to the context for a key value passed to this fucntion
 func Set(key interface{}, value interface{}) {
 
+	if key == nil || value == nil {
+		return
+	}
 	mutex.Lock()
 
 	contextStore[key] = value
@@ -35,6 +38,10 @@ func Set(key interface{}, value interface{}) {
 
 //get the corresponidng value of a key passed to this fucntion
 func Get(key interface{}) (interface{}, bool) {
+
+	if key == nil {
+		return nil, false
+	}
 	mutex.Lock()
 	if value, ok := contextStore[key]; ok {
 		log.Println("Found store....")
@@ -49,12 +56,16 @@ func Get(key interface{}) (interface{}, bool) {
 
 //delete a aprticualr key,value from the context
 func Erase(key interface{}) {
+	mutex.Lock()
 	delete(contextStore, key)
+	mutex.Unlock()
 }
 
 //Delete all the entries fro mthe context
-func celar() {
+func Celar() {
+	mutex.Lock()
 	for key := range contextStore {
 		delete(contextStore, key)
 	}
+	mutex.Unlock()
 }
